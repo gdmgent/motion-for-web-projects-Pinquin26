9cd2b48648704a44d92644a027a9ca0a3fb5f606
Pinquin26 <quinten2626@gmail.com>
2023-06-01 21:59:05 +0200

    ja

--- Eindopdracht-QuintenDT/js/quintendt.js
+++ Eindopdracht-QuintenDT/js/quintendt.js
@@ -15,9 +15,12 @@ function quintendt (fase) {
             height:"5rem",
             rotation: 360,
             borderTop: "solid white 4px",
-            center: 0
+            center: 0,
         }, "football")
-        
+        .to("#quintendt .football-texture",{
+            opacity:0.7,
+            rotation:180
+        }, "football")
         .add("toField")
         .to("#quintendt .box", {
             x:"45vw",
@@ -42,21 +45,16 @@ function quintendt (fase) {
             ease:"none",
             delay:2
         },"run")
-
-        
         
         .from ("#quintendt .history", {
             x:-800,
-            delay:2
+            delay:1.5
         },"run")
-
-
 
         .from ("#quintendt .facts", {
             x:800,
             delay:2
         },"run")
-
 
         .add("superbowl")
         .from("#quintendt .superbowl", {
@@ -67,37 +65,62 @@ function quintendt (fase) {
         .from("#quintendt .img1", {
             y:"-200vh",
             opacity:0,
-            delay:2,
-            ease:"easeOut"
+            ease:"easeOut",
+            delay:3
         },"vs")
 
         .from("#quintendt .img2", {
             y:"200vh",
             opacity:0,
-            delay:2,
-            ease:"easeOut"
+            ease:"easeOut",
+            delay:3
         },"vs")
 
         .add("flyin")
         .from ("#quintendt .AFC", {
             x:"90vw",
             y:"-80vh",
-            rotation:720
+            rotation:720,
+            delay:"-.5"
         },"flyin")
 
         .from ("#quintendt .NFL", {
             x:"-90vw",
             y:"-80vh",
-            rotation:720
+            rotation:720,
+            delay:"-.5"
         },"flyin")
 
-
-        //terug naar wit vierkant
+        .add("goal")
         .to("#quintendt .box", {
+            x:"0vw",
+            y:"10vh",
+            center:0,
+            borderRadius:"0",
+            backgroundColor: "white",
             width:"20rem",
             height:"20rem",
+            rotation: 360,
+            border:"none",
+            delay:2
+
+        }, "goal")
+        .to("#quintendt .football-texture", {
+            //doe met Zindex
+
+        }, "goal")
+        
+        
+        .add("reset")
+        .to("#quintendt .box", {
             borderRadius:"0",
-            backgroundColor: "white"
-        });
+            backgroundColor: "white",
+            width:"20rem",
+            height:"20rem",
+            rotation: 360,
+            border:"none",
+            center: 0,
+        }, "reset")
+
     }
 }
