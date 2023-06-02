1a217eac67081071ec97b994393bcde26d31e7c6
Pinquin26 <quinten2626@gmail.com>
2023-06-02 14:48:41 +0200

    eindopdracht af?

--- Eindopdracht-QuintenDT/js/quintendt.js
+++ Eindopdracht-QuintenDT/js/quintendt.js
@@ -5,8 +5,26 @@ function quintendt (fase) {
     if( !section.init){
         section.init= true;
         console.log("quintendt.js: init");
-        section.tl
-        
+        section.tl = gsap.timeline({
+
+        scrollTrigger : {
+
+        trigger: section,
+
+        scrub: 1,
+
+        start: "top 50%",
+
+        end: "bottom 50%",
+
+        toggleActions: "play none none reverse",
+
+
+
+
+      }});
+      section.tl
+        //add1------------------------------
         .add ("football")
         .to("#quintendt .box", {
             borderRadius:"50%",
@@ -16,22 +34,16 @@ function quintendt (fase) {
             rotation: 360,
             borderTop: "solid white 4px",
             center: 0,
+            x:"45vw",
+            y:"45vh"
         }, "football")
         .to("#quintendt .football-texture",{
             opacity:0.7,
             rotation:180
-        }, "football")
-        .add("toField")
-        .to("#quintendt .box", {
-            x:"45vw",
-            y:"45vh"
-        },"toField")
+        }, "football")        
 
-        .add ("down")
-        .to("#quintendt .box", {
-            y:"60vh"
-        },"down")
 
+        //add2------------------------------
         .add ("catch")
         .to("#quintendt .box", {
             x:"-50vw",
@@ -39,88 +51,110 @@ function quintendt (fase) {
             scale:.3,
         },"catch")
 
-        .add("run")
+
         .to ("#quintendt .gif", {
             x:1200,
-            ease:"none",
-            delay:2
-        },"run")
+            ease:"none"
+        },"catch")
         
         .from ("#quintendt .history", {
-            x:-800,
-            delay:1.5
-        },"run")
+            x:-800
+        },"catch")
 
         .from ("#quintendt .facts", {
-            x:800,
-            delay:2
-        },"run")
+            x:800
+        },"catch")
 
+
+        //add3------------------------------
         .add("superbowl")
-        .from("#quintendt .superbowl", {
-            
+        .to("#quintendt .box", {
+            scale:1
         },"superbowl")
 
+
+        //add4------------------------------
         .add("vs")
         .from("#quintendt .img1", {
             y:"-200vh",
             opacity:0,
             ease:"easeOut",
-            delay:3
         },"vs")
 
         .from("#quintendt .img2", {
             y:"200vh",
             opacity:0,
             ease:"easeOut",
-            delay:3
         },"vs")
 
-        .add("flyin")
         .from ("#quintendt .AFC", {
             x:"90vw",
             y:"-80vh",
-            rotation:720,
-            delay:"-.5"
-        },"flyin")
+            rotation:720
+        },"vs")
 
         .from ("#quintendt .NFL", {
             x:"-90vw",
             y:"-80vh",
-            rotation:720,
-            delay:"-.5"
-        },"flyin")
+            rotation:720
+        },"vs")
 
+
+        //add5------------------------------
         .add("goal")
         .to("#quintendt .box", {
             x:"0vw",
-            y:"10vh",
-            center:0,
-            borderRadius:"0",
-            backgroundColor: "white",
-            width:"20rem",
-            height:"20rem",
-            rotation: 360,
-            border:"none",
-            delay:2
-
+            y:"25vh",
+            scale:.4
         }, "goal")
-        .to("#quintendt .football-texture", {
-            //doe met Zindex
 
-        }, "goal")
-        
-        
-        .add("reset")
+
+        //add6------------------------------
+        .add("score")
+        .to("#quintendt .score", {
+            opacity:1
+        }, "score")
+        .to("#quintendt .box", {
+            rotation:90
+        }, "score")
+
+
+        //add7------------------------------
+        .add("rugbybalText")
+        .from("#quintendt .rugbybal",{
+            x:1000,
+        }, "rugbybalText")
+        .to("#quintendt .box", {
+            x:"40vw",
+            rotation:180
+        }, "rugbybalText")
+
+        //add8------------------------------
+        .add("name")
+        .from("#quintendt .name",{
+            x:1000,
+        }, "name")
+
         .to("#quintendt .box", {
             borderRadius:"0",
             backgroundColor: "white",
-            width:"20rem",
-            height:"20rem",
+            width:"20vw",
+            height:"20vw",
+            scale:1,
             rotation: 360,
             border:"none",
             center: 0,
-        }, "reset")
+            x:"0vw"
+        }, "name")
+        .to("#quintendt .football-texture", {
+            opacity:0
+        },"name")
 
+        //add9------------------------------
+        .add("end")
+        .to("#quintendt .box",{
+            top:"0vh",
+            left:"40vw"
+        },"end")
     }
 }
